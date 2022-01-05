6d2081e2c5eca4113b9a89a8f3bebf391b3a9fc9
Aleksandr Denisevich <denisevich.aa@gmail.com>
2022-01-06 00:13:27 +0300

    feat: added ga

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import "./theming/styled-hook"
+import ReactGA from "react-ga"
 import { IndexPage } from "./components/pages"
 import { GlobalStyles } from "./global-styles"
 import { connector } from "./business/blockchain"
 import { ThemingProvider } from "./theming/provider"
+
+ReactGA.initialize("G-81269FZ9H9")
 
 ReactDOM.render(
   <React.StrictMode>
