0ab51ecaa44290113a0120d6a61f5d5387e0483b
Eugene Nacu <evgeny.nacu@gmail.com>
2022-01-10 23:06:21 +0300

    feat: use KeyMemo instead of KeyCache

--- src/business/domain.ts
+++ src/business/domain.ts
@@ -1,10 +1,5 @@
-import type { Observable } from "rxjs"
-import type { Wrapped } from "@rixio/wrapped"
-
 export type Option<K = string, T extends object = {}> = {
   label: string
   value: K
   data: T
 }
-
-export type OW<T> = Observable<Wrapped<T>>
